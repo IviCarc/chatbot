8194590e5ac34c6564871ac69e529d2e1268b85e
IviCarc <kovachivanagustin@gmail.com>
2023-06-22 13:06:30 -0300

    add template

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__) 
 
 @app.route('/')
 def index():
     return "TEST 3"
+    # return render_template('index.html')
 
 @app.route("/post", methods = ["POST"])
 def agenda():
