7b60cdd150e745ecdf18c24930252e1163d7100a
Benit0Trdlla <benilorenzo007op@gmail.com>
2023-06-23 15:47:35 -0300

    agendar reunion terminado, con funciones suplementarias y delete

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 
-from controllers import getAllReuniones,getReunionById, agendarReunion, editarReunion, eliminarReunion
+from controllers import getAllReuniones,getReunionById, getReunionsByCliente, agendarReunion, editarReunion, eliminarReunion
 app = Flask(__name__)
 
 # API
 app.add_url_rule('/', 'getAllReuniones',getAllReuniones, methods=["GET"])
 app.add_url_rule('/reunion', 'getReunionById',getReunionById, methods=["GET"])
+app.add_url_rule('/cliente', 'getReunionsByCliente',getReunionsByCliente, methods=["GET"])
 app.add_url_rule('/', 'agendarReunion',agendarReunion, methods=["POST"])
 app.add_url_rule('/', 'editarReunion',editarReunion, methods=["PUT"])
 app.add_url_rule('/', 'eliminarReunion',eliminarReunion, methods=["DELETE"])
--- src/backend/controllers.py
+++ src/backend/controllers.py
@@ -3,6 +3,10 @@ import db as db
 import json
 import datetime
 
+from datetime import datetime
+
+### METODOS GET ###
+
 # Las siguientes dos funciones reciben un array de tuplas desde un cursor.fetchall normalmente.
 # Lo que consiguen es devolver un json cuyas keys son los mismos nombres de las columnas en la DB
 # Puede mejorar la complejidad, posiblemente es muy lenta debido al uso de ZIP y MAP
@@ -46,21 +50,66 @@ def getReunionById():
 
     return serialize(reunion, headers)
 
-# def getReunionByCliente():
-    # cursor = db.database.cursor()
-    # id = request.args.get("id")
-    # cursor.execute("SELECT * FROM reuniones WHERE ID = %s", [id])
-    # row_headers=[x[0] for x in cursor.description]
-    # reunion = cursor.fetchall()
+def getReunionsByCliente():
+    cursor = db.database.cursor()
+    cliente = request.args.get("cliente")
+    cursor.execute("SELECT ID FROM clientes WHERE nombre = %s", [cliente])
+    id = cursor.fetchone()[0]
+    cursor.execute("SELECT * FROM reuniones WHERE ID = %s", [id])
 
-    # return serialize(reunion, row_headers)
+    row_headers=[x[0] for x in cursor.description]
+    reuniones = cursor.fetchall()
+
+    return serialize(reuniones, row_headers)
+
+### METODOS POST ###
+
+def guardarCliente(cliente,correo, cursor):
+    print(cliente)
+    cursor.execute("INSERT INTO clientes (nombre, correo) values (%s, %s)", [cliente, correo])
+    db.database.commit()
+    print(cursor.lastrowid)
+    return cursor.lastrowid
+
+def guardarAnfitrion(anfitrion, correo, cursor):
+    cursor.execute("INSERT INTO anfitriones (nombre, correo) values (%s, %s)", [anfitrion, correo])
+    db.database.commit()
+    print(cursor.lastrowid)
+    return cursor.lastrowid
 
 def agendarReunion():
     req = request.get_json()
+    cliente, correoCliente, anfitrion, correoAnfitrion, chat, fecha, hora = req.values()
+
+    fecha = datetime.strptime(fecha, '%Y-%m-%d').date()
+    hora = datetime.strptime(hora, '%H:%M:%S').time()
+
+    cursor = db.database.cursor()   
+    # Chequear si el cliente existe
+    cursor.execute("SELECT ID FROM clientes WHERE nombre = %s", [cliente])
+    ID_cliente = cursor.fetchone()[0]
+
+    if (ID_cliente == None):
+        ID_cliente = guardarCliente(cliente, correoCliente, cursor)
+
+    # Chequear si el anfitrion existe
+    cursor.execute("SELECT ID FROM anfitriones WHERE nombre = %s", [anfitrion])
+    ID_anfitrion = cursor.fetchone()[0]
+
+    if (ID_anfitrion == None):
+        ID_anfitrion = guardarAnfitrion(anfitrion, correoAnfitrion, cursor)
+
+    #Insertar la reunión
+    cursor.execute("INSERT INTO reuniones (chat, fecha, hora, ID_cliente, ID_anfitrion) VALUES (%s, %s, %s, %s, %s)", (chat, fecha, hora, ID_cliente, ID_anfitrion))
+    db.database.commit()
     return req
 
 def editarReunion():
     return "REUNION EDITADA"
 
 def eliminarReunion():
+    cursor = db.database.cursor()
+    id = request.args.get("id")
+    cursor.execute("DELETE FROM reuniones WHERE ID = %s", [id])
+    db.database.commit()
     return "REUNION ELIMINADA"
